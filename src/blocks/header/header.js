f3d737fcbf6266a3ecf788d5b42d6d3ccf4488c7
Tanya Shtatnykh <tanya.shtatnykh@gmail.com>
2023-03-19 22:48:21 +0700

    Added a button to the envelope

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,6 +1,27 @@
 $(function () {
   $('.header__burger-btn').on('click', function() {
+    if ($('.request--header').hasClass('request--header--active')) {
+      $('.request--header').removeClass('request--header--active');
+      $('.header__logo-link').removeClass('header__logo-link--hidden');
+    }
     $(this).toggleClass('header__burger-btn--open');
     $('.header__info').toggleClass('header__info--open');
+  });
+});
+
+$(function () {
+  $('.header__mail').on('click', function() {
+    if ($('.header__burger-btn').hasClass('header__burger-btn--open')) {
+      $('.header__burger-btn').removeClass('header__burger-btn--open');
+      $('.header__info').removeClass('header__info--open');
+    }
+
+    $('.request--header').toggleClass('request--header--active');
+
+    if ($('.request--header').hasClass('request--header--active')) {
+      $('.header__logo-link').addClass('header__logo-link--hidden');
+    } else {
+      $('.header__logo-link').removeClass('header__logo-link--hidden');
+    }
   });
 });
